f00a4ce03e43d970f231dd13637bc1d290991e0f
Eng Sung <engxsung@gmail.com>
2021-05-31 21:46:34 -0400

    added leetcode questions and organized by difficulty

--- background.js
+++ background.js
@@ -1,10 +1,15 @@
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) { 
     // redirect
-    if (details.url.startsWith('http://facebook.com/')) {
+    const targetUrl = details.url;
+    if (targetUrl.includes("facebook.com")){
       return {redirectUrl: 'https://leetcode.com/problems/first-missing-positive/'};
     }
+    return {redirectUrl: 'https://youtube.com/'};
   },
-  {urls: ["*://*.facebook.com/*"]},
+  {
+    urls: ["*://*.facebook.com/*", "*://*.instagram.com/*"],
+    types: ['main_frame', 'sub_frame']
+  },
   ["blocking"]
 );
