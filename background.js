8a2ed56a051650a437bc9468563d8a814c93608b
Eng Sung <engxsung@gmail.com>
2021-06-02 01:07:40 -0400

    redirect users to leetcode

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ let questions = {};
 let targetUrl = '';
 
 /*
-On install, load the leetcode urls intro questions
+Description: on install, load the leetcode urls into questions
  */
 chrome.runtime.onInstalled.addListener(function (details) {
   fetch(chrome.extension.getURL('./questions/questions.json'))
@@ -12,12 +12,15 @@ chrome.runtime.onInstalled.addListener(function (details) {
   });
 });
 
+/*
+Description: listen for webrequests and redirect them to leetcode
+ */
 chrome.webRequest.onBeforeRequest.addListener(
   function(details) { 
     // redirect
     targetUrl = details.url;
     let leetcode = "https://leetcode.com/problems/two-sum";
-    if (targetUrl.includes("facebook.com")){
+    if (targetUrl.includes("chess.com") || targetUrl.includes("netflix.com") || targetUrl.includes("twitch.tv")){
       leetcode = questions.hard[Math.floor(Math.random() * questions.hard.length)];
     } else {
       let combined = questions.easy.concat(questions.medium);
@@ -27,7 +30,7 @@ chrome.webRequest.onBeforeRequest.addListener(
     return {redirectUrl: leetcode};
   },
   {
-    urls: ["*://*.facebook.com/*", "*://*.instagram.com/*"],
+    urls: ["*://*.facebook.com/*", "*://*.instagram.com/*", "*://*.chess.com/*", "*://*.netflix.com/*", "*://*.twitch.tv/*"],
     types: ['main_frame', 'sub_frame']
   },
   ["blocking"]
