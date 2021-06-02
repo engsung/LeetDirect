f0f09aadb41db75d151978c39595d6a0da16c49a
Eng Sung <engxsung@gmail.com>
2021-06-02 00:42:15 -0400

    changing leetcode difficulty

--- content.js
+++ content.js
@@ -1,2 +1,14 @@
-let x = document.getElementsByClassName('css-t42afm');
-console.log(x.length);
+const changeDiff = () => {
+    setTimeout(() => {
+      const node = document.querySelector('.css-t42afm');
+      if (!node) {
+          return
+      } else {
+          node.innerHTML = 'Easy';
+          node.style.color = '#43A047';
+      }
+      
+    }, 100);
+  };
+
+  window.addEventListener('load', changeDiff);
